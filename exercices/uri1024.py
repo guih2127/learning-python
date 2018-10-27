a0ea30f68c4bb3dab0ec97076939fe163db8696f
guih2127 <guih2127@gmail.com>
2018-10-27 14:44:23 -0300

    fucking myself to do exercice1024

--- exercices/uri1024.py
+++ exercices/uri1024.py
@@ -1,26 +1,48 @@
-linha = int(input())
-lista_string = []
+numero_casos = int(input())
+saida = []
+nova_palavra = []
+nova_palavra2 = []
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+ASCII = ''.join(chr(x) for x in range(128))
+ASCII = ASCII.split()
+print(ASCII)
 
-for i in range (0, linha):
-    texto = input()
-    lista_string.append(texto)
-
-print(lista_string)
-
-for i in range (0, linha):
-    palavra = list(lista_string[i])
-
+for i in range(numero_casos):
+    palavra = input()
     for letra in palavra:
-        if letra.isalpha():
-            letra = chr(ord(palavra[i]) + 3)
-    print(letra)
+        if letra in letras:
+            if letra == 'x':
+                nova_letra = '{'
+            elif letra == 'X':
+                nova_letra = '['
+            elif letra == 'y':
+                nova_letra = '|'
+            elif letra == 'Y':
+                nova_letra = '\\'
+            elif letra == 'z':
+                nova_letra = '}'
+            elif letra == 'Z':
+                nova_letra = ']'
+            else:
+                for i in range(0, len(letras)):
+                    if letra == letras[i]:
+                        nova_letra = letras[i + 3]
+                        break
+            nova_palavra.append(nova_letra)
+    
+    inverter = reversed(nova_palavra)
+    for letra in inverter:
+        nova_palavra2.append(letra)
 
-'''for i in range (0, len(lista_string)):
-    for i in range(0, len(lista_string[i]))
-        if lista_string[i].isalpha():
-            lista_string[i] = chr(ord(lista_string[i]) + 3)
+    tamanho_palavra = len(nova_palavra2)
+    tamanho_por_dois = int(tamanho_palavra / 2) * (-1)
 
-print(lista_string)'''
+    for i in range(-1, tamanho_por_dois):
+        for j in range(0, len(ASCII)):
+            if nova_palavra2[i] == ASCII[j]:
+                nova_palavra2[i] = ASCII[j - 1]
 
-
-
+print(nova_palavra2)
