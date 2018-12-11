2ee6c6d49ba3703191205091a6937b2c833a8277
guih2127 <guih2127@gmail.com>
2018-12-11 14:13:21 -0200

    part 4 views and generic views

--- projects/pollsapi/polls/urls.py
+++ projects/pollsapi/polls/urls.py
@@ -2,10 +2,24 @@
 # inicialmente, a API terá dois endpoints que retornarão dados
 # em JSON, /polls/ e /polls/<id>/
 
+# from django.urls import path
+# from .views import polls_list, polls_detail
+
+# 1.
+# urlpatterns = [
+#     path("polls/", polls_list, name="polls_list"),
+#     path("", polls_list, name="polls_list"),
+#     path("polls/<int:pk>/", polls_detail, name="polls_detail")
+# ]
+
+# 2.
 from django.urls import path
-from .views import polls_list, polls_detail
+from .views import PollList, PollDetail, ChoiceList, CreateVote
 
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
 ]
--- projects/pollsapi/polls/views.py
+++ projects/pollsapi/polls/views.py
@@ -2,27 +2,78 @@
 # inicialmente, escrevermos duas view functions para os
 # dois endpoints que definimos.
 
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
+from rest_framework import generics
 
-from .models import Poll
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
-def polls_list(request):
-    MAX_OBJECTS = 20
-    polls = Poll.objects.all()[:MAX_OBJECTS]
-    data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
-    return JsonResponse(data)
+# 1.
+# def polls_list(request):
+#     MAX_OBJECTS = 20
+#     polls = Poll.objects.all()[:MAX_OBJECTS]
+#     data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
+#     return JsonResponse(data)
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data = {"results": {
-        "question": poll.question,
-        "created_by": poll.created_by.username,
-        "pub_date": poll.pub_date
-    }}
-    return JsonResponse(data)
+# def polls_detail(request, pk):
+#     poll = get_object_or_404(Poll, pk=pk)
+#     data = {"results": {
+#         "question": poll.question,
+#         "created_by": poll.created_by.username,
+#         "pub_date": poll.pub_date
+#     }}
+#     return JsonResponse(data)
 
-# inicialmente, escrevemos views utilizando o django, normal, sem utilizar o DRF,
-# essas views utilizam o JsonResponse para retornar os dados em JSON.
-# porém, precisaremos utilizar o DRF, que provém para nós um conjunto enorme de 
-# componentes e coisas convenientes para criar APIs.
+# # inicialmente, escrevemos views utilizando o django, normal, sem utilizar o DRF,
+# # essas views utilizam o JsonResponse para retornar os dados em JSON.
+# # porém, precisaremos utilizar o DRF, que provém para nós um conjunto enorme de 
+# # componentes e coisas convenientes para criar APIs.
+
+# 2.
+# class PollList(APIView):
+#     def get(self, request):
+#         polls = Poll.objects.all()[:20]
+#         data = PollSerializer(polls, many=True).data
+#         return Response(data)
+
+# class PollDetail(APIView):
+#     def get(self, request, pk):
+#         poll = get_object_or_404(Poll, pk=pk)
+#         data = PollSerializer(poll).data
+#         return Response(data)
+
+# Utilizamos a classe APIView, já uma classe do Django Rest, para criarmos
+# nossa API de forma mais limpa.
+# porém, podemos ir um pouco mais, e utilizar as views genéricas do DRF
+# para criar as views da nossa API.
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+
+# aqui, definimos uma queryset, que determina a queryset inicial,
+# e posteriormente pode ser filtrada, ordenada, etc.
+# a serializer_class é o serializer que iremos utilizar para serializar
+# a nossa saída e desserializar a entrada de dados.
+
+# quanto as classes genéricas, elas tem algumas diferenças entre si:
+# ListCreateAPIView: Permite apenas GET e POST, permite listar entidades ou criar uma.
+# RetrieveDestroyAPIView: Permite apenas GET e DELETE, ou seja, ver ou deletar uma entidade.
+# CreateAPIView: Permite apenas o metódo POST, ou seja, permite criação de entidades mas
+# não permite listá-las.
+
+
+
+
+
+
