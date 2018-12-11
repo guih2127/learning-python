ed8e69c499682353ef828f19f282ecd44f415789
guih2127 <guih2127@gmail.com>
2018-12-11 00:18:05 -0200

    finish part 2, views and urls with normal django

--- projects/pollsapi/polls/urls.py
+++ projects/pollsapi/polls/urls.py
@@ -1,2 +1,11 @@
+# aqui ficam as urls que utilizaremos para mapear a API
+# inicialmente, a API terá dois endpoints que retornarão dados
+# em JSON, /polls/ e /polls/<id>/
+
+from django.urls import path
+from .views import polls_list, polls_detail
+
 urlpatterns = [
+    path("polls/", polls_list, name="polls_list"),
+    path("polls/<int:pk>/", polls_detail, name="polls_detail")
 ]
--- projects/pollsapi/polls/views.py
+++ projects/pollsapi/polls/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# nas views ficam a "lógica" que utilizaremos no projeto,
+# inicialmente, escrevermos duas view functions para os
+# dois endpoints que definimos.
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .models import Poll
+
+def polls_list(request):
+    MAX_OBJECTS = 20
+    polls = Poll.objects.all()[:MAX_OBJECTS]
+    data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
+    return JsonResponse(data)
+
+def polls_detail(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    data = {"results": {
+        "question": poll.question,
+        "created_by": poll.created_by.username,
+        "pub_date": poll.pub_date
+    }}
+    return JsonResponse(data)
+
+# inicialmente, escrevemos views utilizando o django, normal, sem utilizar o DRF,
+# essas views utilizam o JsonResponse para retornar os dados em JSON.
+# porém, precisaremos utilizar o DRF, que provém para nós um conjunto enorme de 
+# componentes e coisas convenientes para criar APIs.
